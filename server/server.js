5e7ae1ee13b996321f2076b9783b375e6a1e3ea3
Abhinav <abhimonga16@gmail.com>
2019-06-15 15:25:19 +0530

    connection added

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const path=require('path');
 const reqPath=path.join(__dirname+'/../public');
+const socket=require('socket.io');
+const http=require('http');
 const port=process.env.PORT||3000;
 const express=require('express');
   var app=express();
-
+var server=http.createServer(app);
+var io=socket(server);
 app.use(express.static(reqPath));
-app.listen(port,()=>{
+io.on("connection",(socket)=>{
+ console.log('New user connected');
+ socket.on('disconnect',()=>{
+  console.log('New user disconnected');
+ });
+});
+
+server.listen(port,()=>{
   console.log(`System is on port  ${port}`);
 });
