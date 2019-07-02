715b1b8b9854eb3a92225f985333c8de88c91bc3
Abhinav <abhimonga16@gmail.com>
2019-07-02 11:52:41 +0530

    gif feature added

--- server/server.js
+++ server/server.js
@@ -1,56 +1,56 @@
-const path=require('path');
-const reqPath=path.join(__dirname+'/../public');
-const socket=require('socket.io');
-const http=require('http');
-const port=process.env.PORT||3000;
-const express=require('express');
-var {Users}=require('./utils/users');
-const {message}=require('./utils/message');
-const {isValid}=require('./utils/valid.js');
+const path = require('path');
+const reqPath = path.join(__dirname + '/../public');
+const socket = require('socket.io');
+const http = require('http');
+const port = process.env.PORT || 3000;
+const express = require('express');
+var { Users } = require('./utils/users');
+const { message } = require('./utils/message');
+const { isValid } = require('./utils/valid.js');
 
 
-  var app=express();
-var server=http.createServer(app);
-var io=socket.listen(server);
-var user=new Users(); 
+var app = express();
+var server = http.createServer(app);
+var io = socket.listen(server);
+var user = new Users();
 app.use(express.static(reqPath));
 
-io.on("connection",function(socket){
- console.log('New user connected');
+io.on("connection", function(socket) {
+    console.log('New user connected');
 
-socket.on('join',(params,callback)=>{
-   if(!isValid(params.name)|| !isValid(params.room))
-   {
-     return callback('Enter Valid name and room name');
-   }
-    socket.join(params.room);
-    user.removeUser(socket.id);
-    user.addUser(socket.id,params.name,params.room);
-    io.to(params.room).emit('updated list',user.getUserList(params.room));
-    socket.emit('newMessage',message('admin','Welcome to new chat'));
-      io.to(user.room).emit('newMessage',message("admin","New user joined"));
-    
-});
- 
- socket.on('createMessage',(message)=>{
-  var x=user.getUser(socket.id);
-  if(x){
-    io.to(x.room).emit('newMessage',{
-      from:message.from,
-      text:message.text,
-      createdAt:message.createdAt
-   });
-  }
-  
- });
- socket.on('disconnect',()=>{
-    var users=user.removeUser(socket.id);
-    if(users){
-    io.emit('updated list',user.getUserList(user.room));
-    io.to(users.room).emit('newMessage',message("admin",`${users.name} has left`));
- } });
+    socket.on('join', (params, callback) => {
+        if (!isValid(params.name) || !isValid(params.room)) {
+            return callback('Enter Valid name and room name');
+        }
+        socket.join(params.room);
+        user.removeUser(socket.id);
+        user.addUser(socket.id, params.name, params.room);
+        io.to(params.room).emit('updated list', user.getUserList(params.room));
+        socket.emit('newMessage', message('admin', 'Welcome to new chat'));
+        io.to(user.room).emit('newMessage', message("admin", "New user joined"));
+
+    });
+
+    socket.on('createMessage', (message) => {
+        var x = user.getUser(socket.id);
+        if (x) {
+            io.to(x.room).emit('newMessage', {
+                from: message.from,
+                text: message.text,
+                createdAt: message.createdAt
+            });
+        }
+
+    });
+    socket.on('disconnect', () => {
+        var users = user.removeUser(socket.id);
+        if (users) {
+            io.emit('updated list', user.getUserList(user.room));
+            io.to(users.room).emit('newMessage', message("admin", `${users.name} has left`));
+        }
+    });
 });
 
-server.listen(port,()=>{
-  console.log(`System is on port  ${port}`);
+server.listen(port, () => {
+    console.log(`System is on port  ${port}`);
 });
